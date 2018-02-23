a0da582a15cae6fe3c80ef224072793eb9a48122
Michael Munger <michael@highpoweredhelp.com>
2018-02-23 02:20:10 -0500

    Updated Whitelist manager.

--- classes/AuthenticationWhitelistManager.class.php
+++ classes/AuthenticationWhitelistManager.class.php
@@ -7,8 +7,8 @@ class AuthenticationWhitelistManager
 	var $Configs = NULL;
 
 	public function __construct($Configs) {
-		$this->Configs = Configs;
-		
+		$this->Configs = $Configs;
+
 	    $this->add("/login/");
 	    $this->add("/logout/");
 
